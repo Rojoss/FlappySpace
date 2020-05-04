5a18a1b25e16aa1e72edd1a0063e880fd9a77712
Jos <josroossien@hotmail.com>
2020-05-04 12:06:58 +0200

    Load highest unlocked level on game start.

--- src/ui/screens/game/GameScreen.tsx
+++ src/ui/screens/game/GameScreen.tsx
@@ -2,10 +2,27 @@ import * as React from 'react';
 import Screen from '../Screen';
 import { Game } from '../../../game/Game';
 import GameUI from './GameUI';
+import { GetState } from '../../react/store/Store';
+import { Levels } from '../../../levels/Levels';
 
 export default class GameScreen extends React.Component<any, any> {
 
     public componentDidMount(): void {
+        const levelData = GetState().profile.levelData;
+        let totalCrystals: number = 0;
+        for (const key in levelData) {
+            if (levelData.hasOwnProperty(key)) {
+                totalCrystals += levelData[key].crystals;
+            }
+        }
+
+        for (let i = Levels.count; i >= 1; i--) {
+            if (totalCrystals >= Levels.get(i).crystalsToUnlock) {
+                Game.createGame(i);
+                return;
+            }
+        }
+
         Game.createGame(1);
     }
 
